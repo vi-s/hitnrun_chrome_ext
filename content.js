2fc86b0ff089b6b1e53a654b728bbcb220d4f9e1
Vikram Somu <vikster93@gmail.com>
2015-04-28 10:33:35 -0400

    support for insufficient upload / bonus credit warnings

--- content.js
+++ content.js
@@ -18,6 +18,24 @@ chrome.extension.onMessage.addListener(function(message, sender, sendResponse) {
                 upperb = parseFloat(message.data.upperb);
 
             $(document).ready(function() {
+                var surplus_ul_cred = $('#cU').text().match(/[0-9]+\.[0-9]{1,2}\s+[a-zA-Z]{2}/),
+                    surplus_num = surplus_ul_cred.split(' ')[0],
+                    surplus_units = surplus_ul_cred.split(' ').pop(),
+                    surplus_mb = 0;
+
+                // Normalize UL credit units to standard MB units
+                if (surplus_units == 'KB') {
+                    surplus_mb = surplus_num / 1024.0;
+                } else if (surplus_units == 'MB') {
+                    surplus_mb = surplus_num;
+                } else if (surplus_units == 'GB') {
+                    surplus_mb = surplus_num * 1024;
+                } else if (surplus_units == 'TB') {
+                    surplus_mb = surplus_num * 1024 * 1024;                    
+                }
+
+                var current_bonus = $('#cB').text().match(/[0-9]+\.[0-9]{1,2}/),
+                    current_bonus = parseFloat(current_bonus);
 
                 $('.t1 tbody').children('tr').each(function(i, e) {
                     if(i != 0) {
@@ -31,24 +49,35 @@ chrome.extension.onMessage.addListener(function(message, sender, sendResponse) {
                         //kb units, definitely use UL credit
                         if (gap_units == 'KB') {
                             $(ulcred_zap_btn).trigger('click');
+                            surplus_mb -= gap_num / 1024;
                         }
-                        //gb and tb units, definitely do not use UL credit
-                        else if (gap_units == 'GB' || gap_units == 'TB') {
+                        //gb,tb,pb units, definitely do not use UL credit
+                        else if (gap_units == 'GB' || gap_units == 'TB' || gap_units == 'PB') {
                             $(bonus_zap_btn).trigger('click');
+                            current_bonus -= 50;
                         }
                         //case for mb units. must test gap_num
                         else {
                             if (gap_num <= upperb) {
                                 // use UL cred
-                                $(ulcred_zap_btn).trigger('click');                            
+                                $(ulcred_zap_btn).trigger('click'); 
+                                surplus_mb -= gap_num;
                             } else {
                                 // use bonus pts
                                 $(bonus_zap_btn).trigger('click');
+                                current_bonus -= 50;
                             }
                         }
                     }
                 });
 
+                if (surplus_mb * 1024 < lowerb) {
+                    alert('Insufficient upload credit to zap all torrents!')
+                }
+
+                if (current_bonus < 0) {
+                    alert('Insufficient bonus credit to zap all torrents!')
+                }
             });
 
             break;
