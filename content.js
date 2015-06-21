3728dfe3671f2d38581421030e2104e55ccf7786
Vikram Somu <vikster93@gmail.com>
2015-06-21 21:56:46 +0530

    removed comments from content.js

--- content.js
+++ content.js
@@ -29,11 +29,13 @@ chrome.extension.onMessage.addListener(function(message, sender, sendResponse) {
   }
 });
 
+// Zapper class
 function Zapper(lowerb, upperb) {
   this.lowerb = lowerb;
   this.upperb = upperb;
 }
 
+// Zapper prototype methods
 Zapper.prototype = {
   init_zap: function() {
     var _this = this;
@@ -153,115 +155,6 @@ Zapper.prototype = {
     return surplus_mb;
   }
 };
-
-
-// function parse_and_click_hnrs(lowerb, upperb) {
-//   var current_bonus = ud.current_bonus,
-//       surplus_mb = ud.surplus_mb,
-//       lowerb_exceeded = false;
-
-//   // Print out user stats
-//   console.log(current_bonus, surplus_mb);
-
-//   $('.t1 tbody').children('tr').each(function(i, e) {
-//     if(i != 0) {
-//       var row = $(e).children('td'),
-//           gap = $(row[4]).text(),
-//           //the gap num is how much we actually pay to remove the hit'n'run
-//           gap_num = parseFloat(gap.split(' ')[0]),
-//           gap_units = gap.split(' ').pop().toUpperCase(),
-//           bonus_zap_btn = $(row[10]).find("[type='submit']"),
-//           ulcred_zap_btn = $(row[11]).find("[type='submit']");
-
-//       // check for exceeding lowerb
-//       if (surplus_mb / 1000 < lowerb) {
-//         lowerb_exceeded = true;
-//       }
-
-//       //kb units, definitely use UL credit
-//       if (gap_units == 'KB') {
-//         click_ulcred_btn(ulcred_zap_btn, gap_num / 1000)
-//       }
-//       //gb,tb,pb units, definitely do not use UL credit
-//       else if (gap_units == 'GB' || gap_units == 'TB' || gap_units == 'PB') {
-//         click_bonus_btn(bonus_zap_btn);
-//       }
-//       //case for mb units. must test gap_num
-//       else {
-//         // use UL cred
-//         if (gap_num <= upperb) {
-//           click_ulcred_btn(ulcred_zap_btn, gap_num);
-//         } else {
-//           // use bonus pts
-//           click_bonus_btn(bonus_zap_btn);
-//         }
-//       }
-//     }
-
-//     function click_ulcred_btn(ulcred_zap_btn, dec_amt) {
-//       if (!lowerb_exceeded) {
-//         $(ulcred_zap_btn).trigger('click'); 
-//         surplus_mb -= dec_amt;            
-//       }
-//     }
-
-//     function click_bonus_btn(bonus_zap_btn) {
-//       if (current_bonus >= 50) {
-//         $(bonus_zap_btn).trigger('click');
-//         current_bonus -= 50;
-//       }
-//     }
-
-//   });
-
-//   // we are okay calling this here since the .each() above will not be async
-//   check_and_display_warnings(surplus_mb, lowerb, current_bonus);
-// };
-
-// WARNINGS in case we overrand bonus amt or UL lower bounds
-// function check_and_display_warnings(surplus_mb, lowerb, current_bonus) {
-//   if (surplus_mb / 1000 < lowerb) {
-//     alert('Upload credit below lower bound! Insufficient upload credit to zap all torrents.')
-//   }
-
-//   if (current_bonus < 0) {
-//     alert('Insufficient bonus credit to zap all torrents!')
-//   }
-// }
-
-// function grab_user_data() {
-//   return {
-//     current_bonus: grab_bonus(),
-//     surplus_mb: grab_surplus_ul()
-//   }
-// };
-
-// function grab_bonus() {
-//   var current_bonus = $('#cB').text().match(/[0-9]+\.[0-9]{1,2}/)[0],
-//       current_bonus = parseFloat(current_bonus);
-
-//   return current_bonus;
-// };
-
-// function grab_surplus_ul() {
-//   var surplus_ul_cred = $('#cU').text().match(/[0-9]+\.[0-9]{1,2}\s+[a-zA-Z]{2}/)[0],
-//     surplus_num = parseFloat(surplus_ul_cred.split(' ')[0]),
-//     surplus_units = surplus_ul_cred.split(' ').pop(),
-//     surplus_mb = 0;
-
-//   // Normalize UL credit units to standard MB units
-//   if (surplus_units == 'KB') {
-//     surplus_mb = surplus_num / 1000.0;
-//   } else if (surplus_units == 'MB') {
-//     surplus_mb = surplus_num;
-//   } else if (surplus_units == 'GB') {
-//     surplus_mb = surplus_num * 1000;
-//   } else if (surplus_units == 'TB') {
-//     surplus_mb = surplus_num * 1000 * 1000;          
-//   }
-
-//   return surplus_mb;
-// };
 
 //Check jquery version
 // if (typeof jQuery != 'undefined') {  
